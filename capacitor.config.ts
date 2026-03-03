42ef719e4c68e844a569bd3068e2da71ef692e21
unknown <ojasthamkes@gmail.com>
2026-03-03 13:14:43 +0530

    Added Capacitor Android platform and auto-build GitHub action

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+    appId: 'com.aplibhaji.app',
+    appName: 'APLI BHAJI',
+    webDir: 'dist',
+    server: {
+        androidScheme: 'https'
+    }
+};
+
+export default config;
